9af512384e0705087c89a4065ca1e21e8e3d19b2
Michael Bestas <mkbestas@lineageos.org>
2024-06-01 23:54:46 +0700

    sdm845-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- sdm845.mk
+++ sdm845.mk
@@ -354,7 +354,7 @@ PRODUCT_USE_VNDK_OVERRIDE := true
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # VNDK
 PRODUCT_COPY_FILES += \
